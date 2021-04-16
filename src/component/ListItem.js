d896c266b1661f7805d3b550f05726b7f71b14b2
Rosny Ratsianompo <rosny.rat@onja.org>
2021-04-16 09:00:03 +0300

    fixed swipeable

---
+++ src/component/ListItem.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Platform } from 'react-native';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 18,
+        color: '#69696969',
+    },
+    icon: {
+        height: 30,
+        tintColor: '#69696969',
+        ...Platform.select({
+            ios: {
+                tintColor: 'blue',
+            },
+            android: {
+                tintColor: 'red',
+            },
+        })
+    },
+    separator: {
+        flex: 1,
+        height: 1,
+        backgroundColor: "rgba(0, 0, 0, 0.2)"
+    },
+    leftAction: {
+        flex: 1,
+        backgroundColor: '#388e3c',
+        justifyContent: 'center'
+    },
+    actionText: {
+        color: '#fff',
+        fontWeight: '600',
+        padding: 20,
+    }
+});
+export const Separator = () => <View style={styles.separator} />
+const LeftActions = () => (
+    <View style={styles.leftAction}>
+        <Text style={styles.actionText}>Add to card</Text>
+    </View>
+)
+const ListItem = ({ name, onFavoritePress, isFavorite }) => {
+    let starIcon;
+    if(isFavorite) {
+        starIcon = Platform.select({
+            ios: require('../assets/icons/ios-star.png'),
+            android: require('../assets/icons/md-star.png')
+        })
+    } else {
+        starIcon = Platform.select({
+            ios: require('../assets/icons/ios-star-outline.png'),
+            android: require('../assets/icons/md-star-outline.png')
+        })
+    }
+    return (
+        <Swipeable
+            renderLeftActions={LeftActions}
+        >
+            <View style={styles.container}>
+                <Text style={styles.text}>{name}</Text>
+                {onFavoritePress && (
+                    <TouchableOpacity onPress={onFavoritePress}>
+                        <Image 
+                            source={starIcon}
+                            style={styles.icon}
+                            resizeMode="contain"
+                        />
+                    </TouchableOpacity>
+                )}
+            </View>
+        </Swipeable>
+    )
+}
+export default ListItem;
--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,18 +1,53 @@
-import React from 'react'
-
-import { View, Text, SafeAreaView } from 'react-native'
-
-import nachos from '../data/nachos'
+import React, { useState } from 'react';
+import { View, Text, SafeAreaView, ScrollView, FlatList, KeyboardAvoidingView } from 'react-native';
+import {v4 as uuid } from 'uuid';
+import nachos from '../data/nachos';
+import ListItem, { Separator } from '../component/ListItem';
+import AddItem from '../component/AddItem';
 export default () => {
+    const [list, setList] = useState(nachos)
     return (
-        <SafeAreaView>
-            {nachos.map((item)=>{
-
-                return <Text>{item.name}</Text>
-
-            })}
+        <SafeAreaView style={{ flex: 1}}>
+            <KeyboardAvoidingView 
+                style={{ flex: 1}}
+                bahavior="padding"
+            >
+                <FlatList 
+                    data={list}
+                    renderItem={({ item, index }) => (
+                        <ListItem 
+                            name={item.name}
+                            onFavoritePress={() => alert('todo: handle favorite')}
+                            isFavorite={index < 2}
+                        />
+                    )}
+                    keyExtractor={(item) => item.id}
+                    ItemSeparatorComponent={() => <Separator />}
+                    ListHeaderComponent={() => (
+                        <AddItem 
+                            onSubmitEditing={({ nativeEvent: { text } }) => {
+                                setList([{ id: uuid(), name: text }, ...list])
+                            }}
+                        />
+                    )}
+                />
+            </KeyboardAvoidingView>
         </SafeAreaView>
-
     )
-
+    // return (
+    //     <SafeAreaView>
+    //         <ScrollView>
+    //             {nachos.map((item, index)=>(
+    //                 <React.Fragment key={item.id}>
+    //                     <ListItem 
+    //                         name={item.name}
+    //                         onFavoritePress={() => alert('todo: handle favorite')}
+    //                         isFavorite={index < 2}
+    //                     />
+    //                     <Separator />
+    //                 </React.Fragment>
+    //             ))}
+    //         </ScrollView>
+    //     </SafeAreaView>
+    // )
 };
