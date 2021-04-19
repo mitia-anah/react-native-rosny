4896b2af0cbad766cbbc6975cf57cc1062e05848
Rosny Ratsianompo <rosny.rat@onja.org>
2021-04-19 14:03:53 +0300

    added icon

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,19 +1,74 @@
 import React from 'react'
 import CurrentList from '../screens/CurrentList'
+import ItemDetails from '../screens/ItemDetails'
 import {createStackNavigator} from '@react-navigation/stack'
 import {NavigationContainer} from '@react-navigation/native'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import {Text, Image, Platform } from 'react-native'
+import favouritesList from '../screens/favouritesList'
 
 const Stack = createStackNavigator() 
 
 const CurrentListStack = () => {
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="CurrentList" component={CurrentList}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
-  
+        <Stack.Navigator>
+            <Stack.Screen name="CurrentList" component={CurrentList}/>
+                {/* // options={({ route }) => ({ headerTitle: route.params.name })}/> */}
+            <Stack.Screen 
+            name="ItemDetails" 
+            component={ItemDetails}
+            options={({route}) => {
+                return {
+                    headerTitle: () => {
+                        return <Text>{route.params.item.name}</Text>
+                    }
+                }
+            }}
+            />
+        </Stack.Navigator>
+    )  
 };
 
-export default CurrentListStack
+const FavouritesListStack = () => {
+    return (
+       <Stack.Navigator>
+           <Stack.Screen name="FavouritesList" component={favouritesList}/>
+       </Stack.Navigator>
+    )
+}
+const Tab = createBottomTabNavigator()
+
+const Tabs = () => {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+            screenOptions={({route}) => ({
+                tabBarIcon: ({focused, color, }) => {
+                    let image;
+                    if(route.name === 'Shopping List') {
+                        image = Platform.select({
+                            ios: require('../assets/icons/ios-list.png'),
+                            android: require('../assets/icons/md-list.png')
+                        })
+                    } else {
+                        image = Platform.select({
+                            ios: focused ? require('../assets/icons/ios-star.png') : 
+                                require('../assets/icons/ios-star-outline.png'),
+                            android: focused ? require('../assets/icons/md-star.png'): 
+                                require('../assets/icons/md-star-outline.png')
+                        })
+                    }
+                  
+                    return <Image source={image}
+                    resizeMode= 'contain'
+                    style={{width: 25, tintColor: color}}
+                    />
+                }
+            })}>
+                <Tab.Screen name="Shopping List" component={CurrentListStack}/>
+                <Tab.Screen name="FavouritesList" component={FavouritesListStack}/>
+            </Tab.Navigator>
+    </NavigationContainer>
+    )
+}
+export default Tabs
--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -12,7 +12,7 @@ import {useCurrentList} from '../util/ListManager'
 //     AsyncStorage.setItem('@@GroceryList/currentList', JSON.stringify(list))
 // }
 
-export default () => {
+export default ({navigation}) => {
     const {list, loading, addItem, removeItem} = useCurrentList()
     // const [list, setList] = useState([])
     // const [loading, setLoading] = useState(true) 
@@ -60,13 +60,16 @@ export default () => {
             >
                 <FlatList 
                     data={list}
-                    renderItem={({ item, index }) => (
+                    renderItem={({ item, index }) => (  
                         <ListItem 
                             name={item.name}
                             onFavoritePress={() => alert('todo: handle favorite')}
                             isFavorite={index < 2}
                             onAddedSwipe={() => removeItem(item.id)}
                             onDeleteSwipe={() => removeItem(item.id)}
+                            onRowpress={() => {
+                                navigation.navigate('ItemDetails', {item})
+                            }}
                         />
                     )}
                     keyExtractor={(item) => item.id}
